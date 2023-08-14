24fe3e1f179f7264b15f6d8d78710fa395dbbd62
Shey Barpagga <sheybarpagga@gmail.com>
2023-08-14 16:08:51 -0700

    added navbar

--- app/src/main/java/com/example/realtalk/MainActivity.java
+++ app/src/main/java/com/example/realtalk/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.realtalk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.realtalk.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MessageFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()) {
+                case R.id.message:
+                    replaceFragment(new MessageFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.search:
+                    replaceFragment(new SearchFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
